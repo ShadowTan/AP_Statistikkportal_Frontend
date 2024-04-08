5126dcd0a7304eb3b6a6e1dd609daa728eb828d7
Jan E. <jan.elling.h@gmail.com>
2024-04-08 19:26:39 +0200

    Initialization of Project

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+/**
+ * 
+• Hent data fra https://www.ssb.no/statbank/table/11342
+
+• https://data.ssb.no/api/v0/no/console
+
+• Ta inn brukerinput gjennom browseren
+  • Hardkod eller la bruker velge statistikkvariabel
+  • Velg “kommune” eller “fylke” som parameter
+  • To eller flere kommuner (eller fylker)
+  • Hvilke 3 år eller sekvens av år
+
+•Hent data fra SSB til serveren, og utfør beregninger som gir deg
+median, gjennomsnitt, maksimum og minimum for valgte parameter
+
+• Presenter resultatene i browseren i en tabell
+
+ */
+
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-4xl">Hello World</p>
+    </>
+  );
+}
+
+export default App;
